196c79233927e79ff9e02e1a35912fd8b6d44e93
User <User@DESKTOP-U0J37T4>
2023-04-13 18:41:15 +0400

    creat progect

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        private const int NumbersCount = 20;
+        private static readonly Random Random = new();
         static void Main(string[] args)
         {
+            var arr = Enumerable.Range(0, NumbersCount).Select(_ => Random.Next(10)).ToArray();
+            arr = arr.OrderBy(x => x).ToArray();
         }
     }
 }
