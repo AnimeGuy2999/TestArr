19032f8f3ee73d6b1c2e7ce100cb3475534b4e9f
User <User@DESKTOP-U0J37T4>
2023-04-13 19:38:39 +0400

    items sa peir

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.entitis;
+using System;
 using System.Linq;
 
 namespace ConsoleApp1
@@ -9,10 +10,10 @@ namespace ConsoleApp1
         private static readonly Random Random = new();
         static void Main(string[] args)
         {
-            var arr = Enumerable.Range(0, NumbersCount).Select(_ => Random.Next(10)).ToArray();
-            arr = arr.OrderBy(x => x).ToArray();
+            var arr = Enumerable.Range(0, NumbersCount).Select(_ => new Item { Number = Random.Next(10)}) .ToArray();
+            arr = arr.OrderBy(x => x.Number).ToArray();
 
-            Console.WriteLine(string.Join(" ", arr));
+            Console.WriteLine(string.Join(", ", arr.Select(i => $"{i.Number} - {i.Value}")));
 
             Console.ReadKey();
         }
