e920fc15c9211a6c13b6d13e5e41988e7d76e350
User <User@DESKTOP-U0J37T4>
2023-04-13 19:08:27 +0400

    print

---
+++ ConsoleApp1/EnumerbleExtentsions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public static class EnumerbleExtentsions
+    {
+        public static void ForEach<TValue>(this IEnumerable<TValue> values, Action<TValue> action)
+        {
+            foreach (var value in values)
+                action(value);
+        }
+    }
+}
