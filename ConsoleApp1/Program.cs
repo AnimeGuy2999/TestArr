a46ea4b9cc6d058f3d673dd398b31baa63763127
User <User@DESKTOP-U0J37T4>
2023-04-13 18:59:03 +0400

    Cheange ranges

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,13 @@ namespace ConsoleApp1
     class Program
     {
         private const int NumbersCount = 20;
+        private const int NumbersStart = 0;
+        private const int NumbersEnd = 99;
         private static readonly Random Random = new();
+
         static void Main(string[] args)
         {
-            var arr = Enumerable.Range(0, NumbersCount).Select(_ => Random.Next(10)).ToArray();
+            var arr = Enumerable.Range(NumbersStart, NumbersEnd + 1).Select(_ => Random.Next(10)).ToArray();
             arr = arr.OrderBy(x => x).ToArray();
 
             Console.WriteLine(string.Join(" ", arr));
