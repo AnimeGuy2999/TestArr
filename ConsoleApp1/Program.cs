c0ad24221b2cb23351f6d864bd7d956bed6fc3d6
User <User@DESKTOP-U0J37T4>
2023-04-13 19:41:46 +0400

    Random str item part

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,10 +7,12 @@ namespace ConsoleApp1
     class Program
     {
         private const int NumbersCount = 20;
+        private const int VelueLengthStart = 1;
+        private const int VelueLengthEnd = 5;
         private static readonly Random Random = new();
         static void Main(string[] args)
         {
-            var arr = Enumerable.Range(0, NumbersCount).Select(_ => new Item { Number = Random.Next(10)}) .ToArray();
+            var arr = Enumerable.Range(0, NumbersCount).Select(_ => new Item { Number = Random.Next(10), Value = new string('x', Random.Next(VelueLengthStart, VelueLengthEnd))}) .ToArray();
             arr = arr.OrderBy(x => x.Number).ToArray();
 
             Console.WriteLine(string.Join(", ", arr.Select(i => $"{i.Number} - {i.Value}")));
